aa26d2f83ee011a11905f1658b379d12920cbb2e
My.Project <khuong.kyuubi@gmail.com>
2022-04-28 19:21:39 +0700

    cập nhật

---
+++ Test/Main.js
@@ -0,0 +1,17 @@
+// var student_string = '{"name" : "Nguyen Van Cuong", "age" : "26"}'; // Json
+//
+// var student_obj = JSON.parse(student_string); // chuyển json thành object
+// console.log(student_obj);
+// console.log(typeof student_obj);
+//
+//
+// var student_json = JSON.stringify(student_obj);
+// console.log(student_json);
+// console.log(typeof student_json);
+// Lưu file
+//@ts-ignore
+var storage = require('node-persist');
+//you must first call storage.init
+storage.init( /* options ... */);
+storage.setItem('name', "student_json");
+console.log(storage.getItem('name')); // yourname
