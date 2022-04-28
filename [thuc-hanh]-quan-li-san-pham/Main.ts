a1dbd447dd5e491a8b55ff43f439c100592d96e5
My.Project <khuong.kyuubi@gmail.com>
2022-04-28 13:04:54 +0700

    thêm bài thực hành quản lý sản phẩm

---
+++ [thuc-hanh]-quan-li-san-pham/Main.ts
@@ -0,0 +1,14 @@
+import {Product} from "./Product";
+import {ProductManager} from "./ProductManager";
+
+
+let laptop = new Product("Laptop", 20000);
+let iphone = new Product("iPhone", 15000);
+
+let productManager = new ProductManager();
+productManager.add = laptop;
+productManager.add = iphone;
+
+console.log(productManager.getAll);
+
+
